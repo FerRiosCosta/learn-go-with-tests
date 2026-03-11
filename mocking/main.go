290e61bfc7b94509fe3c2ea691c70eac5e70331a
Fernando Rios <fer.rios.costa@gmail.com>
2026-03-11 09:09:50 -0300

    mocking v4 iterator

--- mocking/main.go
+++ mocking/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -28,8 +29,18 @@ func main() {
 	Countdown(os.Stdout, sleeper)
 }
 
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
+	for i := range countDownFrom(3) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
